6b4cf4c85b5f3a1b16e1e118bba7b6b90bf61fd1
DS dcraig Admin <ds_dcraig@na.DigitalSpiffy.com>
2022-01-29 15:50:41 -0700

    Intro to Programming - M1Lab1

---
+++ M1Lab1dc.py
@@ -0,0 +1,8 @@
+# M1 Assignment 2: Intro to Python Programming
+# Written by D. Craig, January 29, 2022
+
+print("Welcome")
+prior_experience = input("What is your prior Python experience? ")
+programming_languages = input("What programming languages have you studied previously?")
+print(prior_experience, programming_languages)
+
